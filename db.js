b0537309258642bd2c1a4913f248512658de27cd
Jwlee134 <67630303+Jwlee134@users.noreply.github.com>
2020-09-23 16:48:30 +0900

    9/23

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 11111,
+    title: `Video awesome`,
+    description: `This is something I love`,
+    views: 22222,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 121212,
+      name: `Jaewon`,
+      email: `sorhd134@gmail.com`,
+    },
+  },
+  {
+    id: 81819,
+    title: `Video perfect`,
+    description: `This is something I love`,
+    views: 22222,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 121212,
+      name: `Jaewon`,
+      email: `sorhd134@gmail.com`,
+    },
+  },
+  {
+    id: 11615,
+    title: `Video super`,
+    description: `This is something I love`,
+    views: 22222,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+    creator: {
+      id: 121212,
+      name: `Jaewon`,
+      email: `sorhd134@gmail.com`,
+    },
+  },
+];
